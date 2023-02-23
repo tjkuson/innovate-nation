649deb609c9d49b288f0e30614c08ef54bd3497f
Tom Kuson <mail@tjkuson.me>
2023-02-23 11:08:30 +0000

    Add cleaning functions

--- src/import.py
+++ src/import.py
@@ -18,3 +18,31 @@ def read_data(file_path):
     else:
         raise ValueError('File type not supported.')
     return df
+
+def get_missing_values(df):
+    """Returns the position of missing values."""
+    return df.isnull()
+
+def repalce_missing_values_with_new_value(df, new_value = 0):
+    """Replaces missing values with new value. Default is 0."""
+    return df.fillna(new_value)
+
+def replace_missing_values_with_mean(df):
+    """Replaces missing values with mean."""
+    return df.fillna(df.mean())
+
+def replace_missing_values_with_median(df):
+    """Replaces missing values with median."""
+    return df.fillna(df.median())
+
+def replace_missing_values_with_mode(df):
+    """Replaces missing values with mode."""
+    return df.fillna(df.mode())
+
+def drop_column_above_ratio_of_missing_values(df, ratio = 0.5):
+    """Drops columns with ratio of missing values above threshold."""
+    return df.dropna(axis = 1, thresh = ratio * len(df))
+
+def drop_row_above_ratio_of_missing_values(df, ratio = 0.5):
+    """Drops rows with ratio of missing values above threshold."""
+    return df.dropna(axis = 0, thresh = ratio * len(df.columns))
